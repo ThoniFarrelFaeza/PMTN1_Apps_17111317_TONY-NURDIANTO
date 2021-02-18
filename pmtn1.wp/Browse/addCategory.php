0d6fa645753293cdaca220b52849b0d639eb3080
ThoniFarrelFaeza <32830017+ThoniFarrelFaeza@users.noreply.github.com>
2021-02-18 11:38:46 +0700

    Delete pmtn1.wp/Browse directory

--- pmtn1.wp/Browse/addCategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db = mysqli_connect("localhost","root","","pmtn1");
-	if(!$db){
-		echo "Database connect error".mysqli_error();
-	}
-    $names = $_POST ['names'];
-    $curDate=date('d/m/Y');
-
-    $db->query("INSERT INTO category(names,create_date)VALUES('".$names."','".$curDate."')");
